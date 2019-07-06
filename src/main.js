01d640f9111d7994e36cd6ce81bd9605228420d4
feyaq13 <feyaq13@gmail.com>
2019-07-07 01:00:58 +0300

    реализует создание заметки

--- src/main.js
+++ src/main.js
@@ -1,13 +1,40 @@
 const createNoteHeader = document.querySelector('.create-note-header');
 const btnCreateNote = document.querySelector('.btn-create-note');
+const createNoteText = document.querySelector('.create-note-text');
+const notesContainer = document.querySelector('.notes-container');
 
 init();
 
 function addNote () {
+  const header = getHeader();
+  const text = getText();
+  const date = getTimeNote();
 
+  const templateMakedNote = document.querySelector('#template-maked-note').content.querySelector('.note');
+  const element = templateMakedNote.cloneNode(true);
+  element.querySelector('.note-header').textContent = header;
+  element.querySelector('.note-text').textContent = text;
+  element.querySelector('.note-text').attributes[1].value = text;
+  element.querySelector('.datetime').attributes[1].value = date;
+  element.querySelector('.datetime').textContent = date;
+  return notesContainer.appendChild(element);
 }
 
 function init () {
   createNoteHeader.focus();
   btnCreateNote.addEventListener('click', addNote);
+}
+
+function getHeader () {
+  return createNoteHeader.value;
+}
+
+function getText () {
+  return createNoteText.value;
+}
+
+function getTimeNote () {
+  let dateCalc = new Date();
+  let dateNote = ` ${dateCalc.toDateString()} ${dateCalc.getHours()}:${dateCalc.getMinutes()}:${dateCalc.getSeconds()}`;
+  return dateNote;
 }
