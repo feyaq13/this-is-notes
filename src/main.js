0b7b89f186bcaff2368966be4cb512b672c685ac
Semyon Fomin <taxnuke@gmail.com>
2019-07-07 13:44:39 +0300

    добавляет фундамент для модели и чуть рефакторит

--- src/main.js
+++ src/main.js
@@ -2,6 +2,29 @@ const createNoteHeader = document.querySelector('.create-note-header');
 const btnCreateNote = document.querySelector('.btn-create-note');
 const createNoteText = document.querySelector('.create-note-text');
 const notesContainer = document.querySelector('.notes-container');
+const hash = murmurHash3.x86.hash128
+
+const notebook = {
+  notes: [],
+  selectedNote: null,
+  addNote: function(title, body) {
+    this.notes.push(new Note(title, body))
+    console.log(this.notes)
+  },
+  selectNote: function(note) {
+    this.selectedNote = note
+  }
+}
+
+class Note {
+  constructor(title, body) {
+    this.id = hash(Math.random().toString());
+    this.title = title;
+    this.body = body;
+    this.createdAt = moment();
+    this.deleted = false
+  }
+}
 
 init();
 
@@ -10,8 +33,10 @@ function addNote () {
   const text = getText();
   const date = getTimeNote();
 
-  const templateMakedNote = document.querySelector('#template-maked-note').content.querySelector('.note');
-  const element = templateMakedNote.cloneNode(true);
+  notebook.addNote(header, text);
+
+  const templateMadeNote = document.querySelector('#template-maked-note').content.querySelector('.note');
+  const element = templateMadeNote.cloneNode(true);
   element.querySelector('.note-header').textContent = header;
   element.querySelector('.note-text').textContent = text;
   element.querySelector('.note-text').attributes[1].value = text;
@@ -21,6 +46,8 @@ function addNote () {
 }
 
 function init () {
+  // разобраться с докой момента, относительные даты, формат вывода и тд
+  moment.locale('ru')
   createNoteHeader.focus();
   btnCreateNote.addEventListener('click', addNote);
 }
@@ -34,7 +61,5 @@ function getText () {
 }
 
 function getTimeNote () {
-  let dateCalc = new Date();
-  let dateNote = ` ${dateCalc.toDateString()} ${dateCalc.getHours()}:${dateCalc.getMinutes()}:${dateCalc.getSeconds()}`;
-  return dateNote;
+  return moment().format('YYYY-MM-DD ddd HH:mm:ss');
 }
