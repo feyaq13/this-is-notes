229a985ba126003aba1dfda327a08bc6883bfae9
feyaq13 <feyaq13@gmail.com>
2019-07-11 14:14:07 +0300

    реализует рендеринг заметок с некоторыми правками

--- src/main.js
+++ src/main.js
@@ -9,19 +9,32 @@ const notebook = {
   selectedNote: null,
   addNote: function (title, body) {
     this.notes.push(new Note(title, body));
-    console.log(this.notes);
+    // console.log(this.notes);
   },
   selectNote: function (note) {
     this.selectedNote = note;
   }
 };
 
+notebook.notes = restoreNotebook();
+
+function restoreNotebook () {
+  if (localStorage.notes) {
+    return JSON.parse(localStorage.notes);
+  }
+  return createNotebook();
+}
+
+function createNotebook () {
+  return [];
+}
+
 class Note {
   constructor (title, body) {
     this.id = hash(Math.random().toString());
     this.title = title;
     this.body = body;
-    this.createdAt = moment();
+    this.createdAt = moment().format('YYYY-MM-DD ddd HH:mm:ss');
     this.deleted = false;
   }
 }
@@ -46,6 +59,22 @@ function addNote () {
   return notesContainer.appendChild(element);
 }
 
+function renderNotebook (userNotebook) {
+  if (!userNotebook.notes.length) {
+    return;
+  }
+  const templateMadeNote = document.querySelector('#template-made-note').content.querySelector('.note');
+  for (let i = 0; i < userNotebook.notes.length; i++) {
+    const noteElement = templateMadeNote.cloneNode(true);
+    noteElement.querySelector('.note-header').textContent = userNotebook.notes[i].title;
+    noteElement.querySelector('.note-text').textContent = userNotebook.notes[i].body;
+    noteElement.querySelector('.note-text').attributes[1].value = userNotebook.notes[i].body;
+    noteElement.querySelector('.datetime').attributes[1].value = userNotebook.notes[i].createdAt;
+    noteElement.querySelector('.datetime').textContent = userNotebook.notes[i].createdAt;
+    notesContainer.appendChild(noteElement);
+  }
+}
+
 function saveNotes (userNotebook) {
   localStorage.notes = JSON.stringify(userNotebook.notes);
 }
@@ -55,10 +84,11 @@ function init () {
   moment.locale('ru');
   createNoteHeader.focus();
   btnCreateNote.addEventListener('click', addNote);
+  renderNotebook(notebook);
 }
 
 function getHeader () {
-  return createNoteHeader.value;
+  return createNoteHeader.value ? createNoteHeader.value : createNoteHeader.placeholder;
 }
 
 function getText () {
